efa98f44edc76772226430a414174ced923e5e67
Ricardo Martinez <ricardo.martinez.monje@gmail.com>
2017-11-10 23:52:21 +0100

    Update CORS headers and .env variables.

--- app/cors/index.js
+++ app/cors/index.js
@@ -2,8 +2,9 @@ module.exports = (req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*")
     res.header('Access-Control-Request-Method', '*')
     res.header('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, DELETE')
-    res.header('Access-Control-Allow-Headers', '*')
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+    //res.header('Access-Control-Allow-Headers', '*')
+    res.header("Access-Control-Allow-Headers", "Authorization, Origin, X-Requested-With, Content-Type, Accept")
+    
     if (req.method === 'OPTIONS') {
         res.writeHead(200)
 
